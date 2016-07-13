e85cfaee829390cef0a9100470fc287708ea707b
zuck007 <pradeepchoudhary009@gmail.com>
2016-07-14 02:05:04 +0530

    final commit maybe

--- C_TextMode/tictactoe.c
+++ C_TextMode/tictactoe.c
@@ -1,16 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-int chk_empty_cell(int empty[])
-{
-    int i;
-    for(i=0;i<9;++i)
-    {
-        if(empty[i])
-            return empty[i];
-    }
-    return 0;
-}
+int empty[9]={0,0,0,0,0,0,0,0,0};
 void help()
 {
     printf("user input should be in format: 1 1 (i.e RowIndex ColIndex ) \n");
@@ -27,8 +18,36 @@ void help()
     }
 
 }
-
-int find_move(char mat[4][5], char ch, int  flagbot)
+void display(char mat[3][4])
+{
+    printf("\n\n");
+    int i,j,k;
+    
+    for(i = 0; i < 3; ++i)
+    {
+        for(k = 1; k <= 3; ++k)
+            printf(" ____");
+        
+        printf("\n");
+        
+        for(j = 0; j < 3; ++j)
+            printf("| %c  ",mat[i][j]);
+        
+        printf("\n");
+    }
+    
+}
+int chk_empty_cell(int empty[])
+{
+    int i;
+    for(i=0;i<9;++i)
+    {
+        if(empty[i])
+            return empty[i];
+    }
+    return -1;
+}
+int find_move(char mat[3][4],char ch, int flag)
 {
     char dig[10];
     int i,j;
@@ -36,33 +55,39 @@ int find_move(char mat[4][5], char ch, int  flagbot)
     const char *pre_win_moves[3]; 
     if(ch == 'X')
     {
-        pre_win_moves[0] = " XX";
-        pre_win_moves[1] = "X X";
-        pre_win_moves[2] = "XX ";
+        pre_win_moves[0] = "_XX";
+        pre_win_moves[1] = "X_X";
+        pre_win_moves[2] = "XX_";
     }
     else
     {    
-        pre_win_moves[0] = " OO";
-        pre_win_moves[1] = "O O";
-        pre_win_moves[2] = "OO ";
+        pre_win_moves[0] = "_OO";
+        pre_win_moves[1] = "O_O";
+        pre_win_moves[2] = "OO_";
     }
 
     //check for upside string match 
     //X O X         X   X
     //  X     ==>   O X O
     //X O O         X   O
-    char tmp_mat[4][5];
-    for(i=0;i<=3;++i)
+    char tmp_mat[3][4];
+    for(i=0;i<3;++i)
     {    
-        for(j=0;j<=3;++j)
+        for(j=0;j<3;++j)
+            {
+            if(mat[i][j] == '_')
+                empty[i*3+j] = 10*i + j;
+            else
+                empty[i*3+j] = 0;
             tmp_mat[i][j] = mat[j][i];
+        }
         tmp_mat[i][j] = '\0';
     }
     
-    for(i=1;i<=3;++i)
+    for(i=0;i<3;++i)
     {
-        dig[i-1] = mat[i][i];
-        inv_dig[i-1] = mat[i][4-i];
+        dig[i] = mat[i][i];
+        inv_dig[i] = mat[i][2-i];
     }
     dig[i] = '\0';
     inv_dig[i] = '\0';
@@ -73,69 +98,86 @@ int find_move(char mat[4][5], char ch, int  flagbot)
     //    X
     
     
-    if( mat[2][2] == ' ')
-        return 22;
+    if( mat[1][1] == '_')
+        return 11;
     if(ch == 'X')
     {
-        if(!flagbot && strcmp("X  ",tmp_mat[1]+1)==0 && strcmp("  X",mat[3]+1)==0)
-        return 31;
-        if(!flagbot && strcmp("  X",tmp_mat[3]+1)==0 && strcmp("X  ",mat[1]+1) == 0)
-        return 13;
+        if(!flag && strcmp("X__",tmp_mat[0])==0 && strcmp("__X",mat[2])==0 ){
+            if(strcmp("_O_",tmp_mat[1])==0)
+                return 01;
+            else
+                return 20;
+        }
+        if(!flag && strcmp("__X",tmp_mat[2])==0 && strcmp("X__",mat[0]) == 0){
+            if(strcmp("_O_",tmp_mat[1])==0)
+                return 01;
+            else
+                return 02;
+        }
+      
     }
     else
-    {
-        if(!flagbot && strcmp("O  ",tmp_mat[1]+1)==0 && strcmp("  O",mat[3]+1)==0)
-            return 31;
-        if(!flagbot && strcmp("  O",tmp_mat[3]+1)==0 && strcmp("O  ",mat[1]+1) == 0)
-            return 13;
- 
+        {
+        if(!flag && strcmp("O__",tmp_mat[0])==0 && strcmp("__O",mat[2])==0){
+            if(strcmp("_O_",tmp_mat[1])==0)
+                return 01;
+            else
+                return 20;
+        }
+        if(!flag && strcmp("__O",tmp_mat[2])==0 && strcmp("O__",mat[0]) == 0){
+            if(strcmp("_O_",tmp_mat[1])==0)
+                return 01;
+            else
+                return 20;
+        }
     }
 
-    for(i=1;i<=3;++i)
+
+    for(i=0;i<3;++i)
     {
 
-        //printf("pre_win_moves[%d-1]=%s ,dig=%s\n",i,pre_win_moves[i-1],dig);
-        //printf("pre_win_moves[%d-1]=%s ,inv_dig=%s\n",i,pre_win_moves[i-1],inv_dig);
-        if(strcmp(pre_win_moves[i-1],dig)==0)
+        //printf("pre_win_moves[%d]=%s ,dig=%s\n",i,pre_win_moves[i],dig);
+        //printf("pre_win_moves[%d]=%s ,inv_dig=%s\n",i,pre_win_moves[i],inv_dig);
+        if(strcmp(pre_win_moves[i],dig)==0)
             return i*10 + i;
-        if(strcmp(pre_win_moves[i-1],inv_dig)==0)
-            return i*10 + 4-i;
+        if(strcmp(pre_win_moves[i],inv_dig)==0)
+            return i*10 + 2-i;
         
         for(j=0;j<3;++j)
         {
             //printf("pre_win_moves[%d]=%s , mat[%d]=%s\n",j,pre_win_moves[j],i,mat[i]);
             //printf("pre_win_moves[%d]=%s , tmp_mat[%d]=%s\n",j,pre_win_moves[j],i,tmp_mat[i]);
-            if(strcmp(pre_win_moves[j],mat[i]+1)==0)
+            if(strcmp(pre_win_moves[j],mat[i])==0)
             {
-                return i*10 + j+1;
+                return i*10 + j;
             }
-            if(strcmp(pre_win_moves[j],tmp_mat[i]+1)==0)
-                return (j+1)*10 + i;
+            if(strcmp(pre_win_moves[j],tmp_mat[i])==0)
+                return j*10 + i;
             
         }
     }
         //random block selection
-    return -1;
+    return chk_empty_cell(empty);
 
 }
 
-int win(char mat[4][5])
+int win(char mat[3][4])
 {
     int i, j;
 
-    if(mat[1][1] == mat[2][2] && mat[2][2] == mat[3][3] && mat[1][1] != ' ')
+    if(mat[0][0] == mat[1][1] && mat[1][1] == mat[2][2] && mat[1][1] != '_')
         return 1;
     
-    if(mat[1][3] == mat[2][2] && mat[2][2] == mat[3][1] && mat[1][3] != ' ')
+    if(mat[0][2] == mat[1][1] && mat[1][1] == mat[2][0] && mat[2][0] != '_')
         return 1;
     
-    for(i = 1; i <= 3; ++i)
+    for(i = 0; i < 3; ++i)
     {
-        char prow = mat[i][1];
-        char pcol = mat[1][i];
+        char prow = mat[i][0];
+        char pcol = mat[0][i];
         int flag_prow = 1;
         int flag_pcol = 1;
-        for(j = 1; j <= 3; ++j)
+        for(j = 0; j < 3; ++j)
         {
            if(mat[i][j] != prow)
            {
@@ -146,35 +188,17 @@ int win(char mat[4][5])
                flag_pcol = 0;
            }
         }
-        if((flag_prow && prow != ' ')|| (flag_pcol && pcol != ' '))
+        if((flag_prow && prow != '_')|| (flag_pcol && pcol != '_'))
             return 1;
     }
     return 0;
 }
-void display(char mat[4][5])
-{
-    printf("\n\n");
-    int i,j,k;
-    
-    for(i = 1; i <= 3; ++i)
-    {
-        for(k = 1; k <= 3; ++k)
-            printf(" ____");
-        
-        printf("\n");
-        
-        for(j = 1; j <= 3; ++j)
-            printf("| %c  ",mat[i][j]);
-        
-        printf("\n");
-    }
-    
-}
+
+
+
 int main()
 {
-    int empty[9] = {11,12,13,21,22,23,31,32,33};
-    int empsize = 9;
-    char mat[4][5]={{' ',' ',' ',' ','\0'},{' ',' ',' ',' ','\0'},{' ',' ',' ',' ','\0'},{' ',' ',' ',' ','\0'}};
+    char mat[3][4]={{'_','_','_','\0'},{'_','_','_','\0'},{'_','_','_','\0'}};
     int ch,i,r,c;
     char player,bot;
     help();
@@ -200,14 +224,15 @@ int main()
     
         printf("Player Move: \n");
         scanf("%d %d",&r,&c);
-        if(!(r>=1 && r<=3 && c>=1 && c<=3 && mat[r][c] == ' '))
+        r-=1;
+        c-=1;
+        if(!(r>=0 && r<3 && c>=0 && c<3 && mat[r][c] == '_'))
         {
             printf("Either Invalid Move Or Block Already Filled (Input Format must be RowIndex ColIndex)  \n\n\n");
             i-=1;
             continue;
         }
         mat[r][c] = player;
-        empty[3*(r-1)+c-1] = 0;
         if(win(mat))
         {
             display(mat);
@@ -223,27 +248,23 @@ int main()
             mat[x/10][x%10] = bot;
             if(win(mat))
             {
-                printf("Bot Move %d %d\n",x/10,x%10);
+                printf("Bot Move %d %d\n",x/10+1,x%10+1);
                 display(mat);
                 printf("\n\nBot Won");
                 exit(0);
             }
             else
-                mat[x/10][x%10] = ' ';
+                mat[x/10][x%10] = '_';
         }
         // stopping player to win in his next move
-        int player_next_move = find_move(mat,player,0);
+        int x = find_move(mat,player,0);
         
-        int x = player_next_move;
-        //printf("x is %d\n",x);
-        if(player_next_move == -1)
+        if(x == -1)
         {
-            x = chk_empty_cell(empty);
-            if(x == -1)
-                break;
+            break;
         }
         
-        printf("Bot Move: %d %d\n",x/10,x%10);
+        printf("Bot Move: %d %d\n",x/10+1,x%10+1);
         mat[x/10][x%10] = bot;
         display(mat);
         if(win(mat))
@@ -252,12 +273,7 @@ int main()
             printf("Bot won\n");
             exit(0);
         }
-        empty[3*(x/10-1)+(x%10-1)] = 0;
-        /*
-        printf("\n1 --- %s\n",mat[1]);
-        printf("\n2 --- %s\n",mat[2]);
-        printf("\n3 --- %s\n",mat[3]);
-        */
+    
     }
    
     printf("\n\n....DRAW......\n");
